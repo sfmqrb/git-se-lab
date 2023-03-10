c19d579abd23e0c789593f733c1210d37a0279a3
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-03-10 12:13:43 +0330

    add summation test
    new test for add function in main

--- src/test.py
+++ src/test.py
@@ -7,3 +7,7 @@ assert add(number1, number2) == number2 + number1, f"numbers are not equal"
 number1 = 122
 number2 = 12 
 assert add(number1, number2) == number2 + number1, f"numbers are not equal"
+
+number1 = 121
+number2 = 5 
+assert add(number1, number2) == number2 + number1, f"numbers are not equal"
