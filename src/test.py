11889ec0d485ab746814321f2f15d42be11e7fe3
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-03-10 12:03:14 +0330

    fix test

--- src/test.py
+++ src/test.py
@@ -2,4 +2,4 @@ from main import add
 
 number1 = 12
 number2 = 2 
-assert add(number1, number2) == number2 + number1, f"numbers are not equal"
+assert add(number1, number2) == number2 * number1, f"numbers are not equal"
