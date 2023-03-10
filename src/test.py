19ff9c0be5618a782800558f5ea21879a0f7cba8
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-03-10 12:04:09 +0330

    add another test
    new test for add function

--- src/test.py
+++ src/test.py
@@ -3,3 +3,7 @@ from main import add
 number1 = 12
 number2 = 2 
 assert add(number1, number2) == number2 + number1, f"numbers are not equal"
+
+number1 = 122
+number2 = 12 
+assert add(number1, number2) == number2 + number1, f"numbers are not equal"
